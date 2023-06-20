3a57c3b1e492e756d0a72f733b00602cc57a91b9
_Kerman <kermanx@qq.com>
2023-06-20 05:28:23 +0000

    update

--- ast.d.ts
+++ ast.d.ts
@@ -1,97 +1,150 @@
-type UnionOfStundents = any
-
-
-interface ASTNode<T>{
-    type: string
-    getValue():T
-}
-type VoidNode = ASTNode<void>
-type UnionNode = ASTNode<UnionOfStundents>
-type IntNode = ASTNode<number>
-type BooleanNode = ASTNode<boolean>
-
-type Context =Record<string,any>;
-
-interface Program extends BooleanNode{
-  statements: Statement[]
+/**
+ * It is not a must to make the value the simplist when parsing.
+ */
+interface UnionOfStundents {
+  individuals: number[];
+  classes: number[];
+  grades: number[];
 }
 
-type Statement = MustStatement | JustStatement | ReturnStatement | AssignmentStatement
+interface ASTNode<T> {
+  type: string;
+  getValue(): T;
+}
+type VoidNode = ASTNode<void>;
+type UnionNode = ASTNode<UnionOfStundents>;
+type IntNode = ASTNode<number>;
+type BooleanNode = ASTNode<boolean>;
 
-interface MustStatement extends VoidNode{
-  type: 'must'
-  expr: BooleanNode
-}
-interface JustStatement extends VoidNode{
-  type: 'just'
-  expr: BooleanNode
-}
-interface ReturnStatement extends VoidNode{
-  type: 'return'
-  expr: BooleanNode
-}
-interface AssignmentStatement extends VoidNode{
-  type: 'assignment'
-  id: string
-  expr: ASTNode<any>
+type Context = Record<string, any>;
+
+interface Program extends BooleanNode {
+  statements: Statement[];
 }
 
-interface UnionLiteral extends UnionNode{
-  type: 'union-literal'
+type Statement =
+  | MustStatement
+  | JustStatement
+  | ReturnStatement
+  | AssignmentStatement;
+
+interface MustStatement extends VoidNode {
+  type: "must";
+  expr: BooleanNode;
 }
-interface NumericLiteral extends IntNode{
-  type: 'numeric-literal'
-  ASTNode: number
+interface JustStatement extends VoidNode {
+  type: "just";
+  expr: BooleanNode;
 }
-interface Id<Ctx extends Context, Id extends keyof Ctx> extends ASTNode<Ctx[Id]>{
-  type: 'id'
-  ASTNode: string
+interface ReturnStatement extends VoidNode {
+  type: "return";
+  expr: BooleanNode;
+}
+interface AssignmentStatement extends VoidNode {
+  type: "assignment";
+  id: string;
+  expr: ASTNode<any>;
 }
 
-interface UnionKindExpr<N extends string> extends UnionNode{
-  type: N
-  left: UnionNode
-  right: UnionNode
+interface UnionLiteral extends UnionNode {
+  type: "union-literal";
 }
-type UnionExpr = UnionKindExpr<'union'>
-type IntersectExpr = UnionKindExpr<'intersect'>
-
-interface AlgebraKindExpr<N extends string> extends IntNode{
-    type: N
-    left: IntNode
-    right: IntNode
+interface NumericLiteral extends IntNode {
+  type: "numeric-literal";
+  ASTNode: number;
 }
-type AddExpr = AlgebraKindExpr<'add'>
-type SubExpr = AlgebraKindExpr<'sub'>
-
-interface ComparisonKindExpr<N extends string> extends BooleanNode{
-    type: N
-    left: IntNode
-    right: IntNode
-}
-type EqExpr = ComparisonKindExpr<'eq'>
-type NeExpr = ComparisonKindExpr<'ne'>
-type GtExpr = ComparisonKindExpr<'gt'>
-type GeExpr = ComparisonKindExpr<'ge'>
-type LtExpr = ComparisonKindExpr<'lt'>
-type LeExpr = ComparisonKindExpr<'le'>
-
-interface NotExpr extends BooleanNode{
-    type: 'not'
-    expr: BooleanNode
+interface Id<Ctx extends Context, Id extends keyof Ctx>
+  extends ASTNode<Ctx[Id]> {
+  type: "id";
+  ASTNode: string;
 }
 
-interface LogicalKindExpr<N extends string> extends BooleanNode{
-    type: N
-    left: BooleanNode
-    right: BooleanNode
+interface UnionKindExpr<N extends string> extends UnionNode {
+  type: N;
+  left: UnionNode;
+  right: UnionNode;
 }
-type AndExpr = LogicalKindExpr<'and'>
-type OrExpr = LogicalKindExpr<'or'>
+type UnionExpr = UnionKindExpr<"union">;
+type IntersectExpr = UnionKindExpr<"intersect">;
 
-type ConverionKindExpr<N extends string, F extends ASTNode<any>, T extends ASTNode<any>> =  T&{
-    type: N
-    from: F
+interface AlgebraKindExpr<N extends string> extends IntNode {
+  type: N;
+  left: IntNode;
+  right: IntNode;
 }
-type UnionToIntExpr = ConverionKindExpr<'union-to-int', UnionNode, IntNode>
-type IntToBooleanExpr = ConverionKindExpr<'int-to-boolean', IntNode, BooleanNode>
+type AddExpr = AlgebraKindExpr<"add">;
+type SubExpr = AlgebraKindExpr<"sub">;
+
+interface ComparisonKindExpr<N extends string> extends BooleanNode {
+  type: N;
+  left: IntNode;
+  right: IntNode;
+}
+type EqExpr = ComparisonKindExpr<"eq">;
+type NeExpr = ComparisonKindExpr<"ne">;
+type GtExpr = ComparisonKindExpr<"gt">;
+type GeExpr = ComparisonKindExpr<"ge">;
+type LtExpr = ComparisonKindExpr<"lt">;
+type LeExpr = ComparisonKindExpr<"le">;
+
+interface NotExpr extends BooleanNode {
+  type: "not";
+  expr: BooleanNode;
+}
+
+interface LogicalKindExpr<N extends string> extends BooleanNode {
+  type: N;
+  left: BooleanNode;
+  right: BooleanNode;
+}
+type AndExpr = LogicalKindExpr<"and">;
+type OrExpr = LogicalKindExpr<"or">;
+
+type ConverionKindExpr<
+  N extends string,
+  F extends ASTNode<any>,
+  T extends ASTNode<any>
+> = T & {
+  type: N;
+  from: F;
+};
+type UnionToIntExpr = ConverionKindExpr<"union-to-int", UnionNode, IntNode>;
+type IntToBooleanExpr = ConverionKindExpr<
+  "int-to-boolean",
+  IntNode,
+  BooleanNode
+>;
+
+export {
+  Program,
+  Statement,
+  MustStatement,
+  JustStatement,
+  ReturnStatement,
+  AssignmentStatement,
+  UnionLiteral,
+  NumericLiteral,
+  Id,
+  UnionExpr,
+  IntersectExpr,
+  AddExpr,
+  SubExpr,
+  EqExpr,
+  NeExpr,
+  GtExpr,
+  GeExpr,
+  LtExpr,
+  LeExpr,
+  NotExpr,
+  AndExpr,
+  OrExpr,
+  UnionToIntExpr,
+  IntToBooleanExpr,
+  Context,
+  UnionOfStundents,
+  VoidNode,
+  UnionNode,
+  IntNode,
+  BooleanNode,
+  ASTNode,
+};
